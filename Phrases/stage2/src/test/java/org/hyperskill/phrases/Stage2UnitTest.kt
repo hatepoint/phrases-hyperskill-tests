57af05e19c039aa9b8886501f1c937593f9d8a0e
Eugene Danileichenko <evgeniy.d48@gmail.com>
2022-11-05 02:47:40 +0200

    Stage 2 tests

---
+++ Phrases/stage2/src/test/java/org/hyperskill/phrases/Stage2UnitTest.kt
@@ -0,0 +1,34 @@
+package org.hyperskill.phrases
+
+import org.hyperskill.phrases.internals.PhrasesUnitTest
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class Stage2UnitTest : PhrasesUnitTest<MainActivity>(MainActivity::class.java){
+
+    @Test
+    fun checkReminderTextView() {
+        testActivity {
+            reminderTv
+        }
+    }
+
+    @Test
+    fun checkRecyclerView() {
+        testActivity {
+            recyclerView
+            recyclerViewItems
+            recyclerViewCheckAmount
+            recyclerViewClick
+        }
+    }
+
+    @Test
+    fun checkFloatingButton() {
+        testActivity {
+            floatingButton
+        }
+    }
+}
