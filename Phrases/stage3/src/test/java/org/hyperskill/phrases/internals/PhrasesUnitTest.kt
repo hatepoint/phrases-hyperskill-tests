c8bfd1ff65d118627c2c770732ab9cae445b99e6
RedJocker <mauricio.guaruja@gmail.com>
2022-11-17 08:36:24 -0300

    removing triggered alarms, #14

--- Phrases/stage3/src/test/java/org/hyperskill/phrases/internals/PhrasesUnitTest.kt
+++ Phrases/stage3/src/test/java/org/hyperskill/phrases/internals/PhrasesUnitTest.kt
@@ -116,7 +116,10 @@ open class PhrasesUnitTest<T : Activity>(clazz: Class<T>): AbstractUnitTest<T>(c
     fun supportForAlarmManager() {
         val alarmManager = activity.getSystemService<AlarmManager>()
         val shadowAlarmManager: ShadowAlarmManager = shadowOf(alarmManager)
-        shadowAlarmManager.scheduledAlarms.lastOrNull()?.also {
+        val toTrigger = shadowAlarmManager.scheduledAlarms.filter {
+            it.triggerAtTime < SystemClock.currentGnssTimeClock().millis()
+        }
+        toTrigger.forEach {
             if(it.operation != null) {
                 val pendingIntent = Shadows.shadowOf(it.operation)
                 if(it.triggerAtTime < SystemClock.currentGnssTimeClock().millis()) {
@@ -134,7 +137,7 @@ open class PhrasesUnitTest<T : Activity>(clazz: Class<T>): AbstractUnitTest<T>(c
                     it.onAlarmListener.onAlarm()
                 }
             }
+            shadowAlarmManager.scheduledAlarms.remove(it)
         }
-
     }
 }
